d829c01c6c8e469ef5554d3321e9612a68a6a103
Panchito1234-ui <alegoieg2007@gmail.com>
2025-01-10 09:45:24 +0100

    Add files via upload

---
+++ REGALITO 🎁/script.js
@@ -0,0 +1,9 @@
+function startExperience() {
+    document.querySelector('.welcome').classList.add('hidden');
+    document.querySelector('.gallery').classList.remove('hidden');
+}
+
+function showMessage() {
+    document.querySelector('.gallery').classList.add('hidden');
+    document.querySelector('.message').classList.remove('hidden');
+}
